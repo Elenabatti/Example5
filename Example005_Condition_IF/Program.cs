1fc94cbd781508b3db8cdd0a3591c3c88a77393d
ELENA <battilena@gmail.com>
2022-08-21 10:21:49 +0300

    add condintion

---
+++ Example005_Condition_IF/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя:");
+        string username = Console.ReadLine();
+
+        if (username == "Маша") ;
+        {
+            Console.WriteLine("Ура, это же Маша");
+        }
+else
+        {
+            Console.Write("Привет, ");
+            Comsole.WriteLine(username);
+        }
+    }
+}
